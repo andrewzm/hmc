fcc3938d6680bda6204f4e67d43ce4d36af5d6f4
andrewzm <andrewzm@gmail.com>
2015-08-25 11:01:02 +1000

    update

--- R/hmc.R
+++ R/hmc.R
@@ -4,6 +4,8 @@
 #' @param M the mass matrix (i.e.,~the covariance matrix in the kinetic energy function)
 #' @param eps_gen a function which generates the leapfrog step-size \eqn{\epsilon}
 #' @param L number of leapfrog steps per proposal
+#' @param lower vector of lower constraints on the variables
+#' @param upper vector of upper constraints on the variables
 #' @examples
 #'
 #' ### Model setup -- 2d correlated Gaussian
@@ -27,13 +29,13 @@
 #' q <- matrix(0,n,N)
 #' for(i in 2:N) q[,i] <- sampler(q = q[,(i-1)])
 #' plot(t(q),ylim = c(-4,4),xlim=c(-4,4))
-hmc_sampler <- function(U,dUdq,M,eps_gen,L) {
-  .check_args(U=U, dUdq = dUdq, M=M, eps_gen = eps_gen, L=L)
+hmc_sampler <- function(U,dUdq,M,eps_gen,L,lower=NULL,upper=NULL) {
+  .check_args(U=U, dUdq = dUdq, M=M, eps_gen = eps_gen, L=L,lower=lower,upper=upper)
   cholM <- chol(M)
   Minv <- chol2inv(cholM)
   function(q) {
     stopifnot(length(q) == nrow(M))
-    .hmc_sample (q, U=U, dUdq = dUdq, Minv = Minv, cholM = cholM, eps_gen=eps_gen, L=L)
+    .hmc_sample (q, U=U, dUdq = dUdq, Minv = Minv, cholM = cholM, eps_gen=eps_gen, L=L,lower=lower,upper=upper)
   }
 }
 
@@ -56,7 +58,7 @@ hmc_sampler <- function(U,dUdq,M,eps_gen,L) {
 #'   qp[i+1,] <- leapfrog(qp = qp[1,], dUdq = dUdq, Minv = Minv,eps = eps_gen(),L=i)
 #' 
 #' plot(qp$q,qp$p)
-leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
+leapfrog <- function(qp,dUdq,Minv,eps,L=1L,lower=NULL,upper=NULL) {
   
   q <- qp$q
   p <- qp$p
@@ -64,6 +66,18 @@ leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
   p <- p - eps/2 * dUdq(q)
   for(i in 1:L) {
     q <- q + eps*Minv %*% p
+    if(!(is.null(lower))) {
+        q_id <- which(q < lower)
+        q[q_id] <- lower[q_id] + (lower[q_id] - q[q_id])
+        p[q_id] <- -p[q_id]
+      }
+        
+    if(!(is.null(upper))) {
+      q_id <- which(q > upper)
+      q[q_id] <- upper[q_id] - (q[q_id] - upper[q_id])
+      p[q_id] <- -p[q_id]
+    }
+    
     if(!(i==L)) p <- p - eps*dUdq(q) 
   }
   p <- p - eps * dUdq(q)/2
@@ -71,21 +85,24 @@ leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
   data.frame(q=q,p=p)
 }
 
+
+.is.diag <- function(X) {
+  all(X[lower.tri(X)] == 0, X[upper.tri(X)] == 0)
+}
 .sample_momentum <- function(cholM) {
   t(cholM) %*% rnorm(n = nrow(cholM))
 }
 
-.propose_state <- function(qp,U,dUdq,Minv,eps,L) {
+.propose_state <- function(qp,U,dUdq,Minv,eps,L,lower,upper) {
   
   #Run the dynamics
-  qp <- leapfrog(qp = qp ,dUdq = dUdq,Minv = Minv,eps = eps,L=L)
-  
+  qp <- leapfrog(qp = qp ,dUdq = dUdq,Minv = Minv,eps = eps,L=L,lower=lower,upper=upper)
   # Negate momentum for summetric proposal
   qp$p <- -qp$p
   qp
 }
 
-.hmc_sample <- function(q,U,dUdq,Minv,cholM,eps_gen,L) {
+.hmc_sample <- function(q,U,dUdq,Minv,cholM,eps_gen,L,lower,upper) {
   
   current_q <- q
   current_p <- .sample_momentum(cholM = cholM)
@@ -93,7 +110,7 @@ leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
   current_K <- 0.5 * t(current_p) %*% Minv %*% current_p 
   qp <- data.frame(q=current_q, p = current_p)
   
-  proposed_qp <- .propose_state(qp,U = U,dUdq = dUdq,Minv = Minv,eps = eps_gen(),L = L)
+  proposed_qp <- .propose_state(qp,U = U,dUdq = dUdq,Minv = Minv,eps = eps_gen(),L = L,lower=lower,upper=upper)
   proposed_U <- U(proposed_qp$q)
   proposed_K <- 0.5 * t(proposed_qp$p) %*% Minv %*% proposed_qp$p
   
@@ -109,7 +126,9 @@ leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
                         dUdq = function(){},
                         M = matrix(c(1,0,0,1),2,2),
                         eps_gen=function() 0.1,
-                        L=10L) {
+                        L=10L,
+                        lower=NULL,
+                        upper=NULL) {
    
   stopifnot(is.data.frame(qp))
   stopifnot(names(qp) == c("q","p"))
@@ -119,5 +138,9 @@ leapfrog <- function(qp,dUdq,Minv,eps,L=1L) {
   stopifnot(is.function(eps_gen))
   stopifnot(is.integer(L))
   stopifnot(L > 0)
-  
+  stopifnot(is.null(lower) | is.numeric(lower))
+  stopifnot(is.null(upper) | is.numeric(upper))
+  if(!(is.null(lower)) & !.is.diag(M)) stop("Constraints not implemented for non-diagonal M")
+  if(!(is.null(upper)) & !.is.diag(M)) stop("Constraints not implemented for non-diagonal M")
+    
 }
